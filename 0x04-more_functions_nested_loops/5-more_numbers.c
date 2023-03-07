1e1290c10aa9c448da24b5ff38cca2baae75958e
Durinfolk <hamdan567tanko@gmail.com>
2023-03-06 17:28:00 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -19,16 +19,3 @@ void more_numbers(void)
 		_putchar('\n');
 	}
 }
-
-#include "main.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-more_numbers();
-return (0);
-}
